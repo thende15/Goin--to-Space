2409028da10bc241a55868c72c17b8fbebaaae62
Timothy Henderson <titansmith8@hotmail.com>
2022-10-06 14:28:21 -0400

    little bit of routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrewComponent } from './components/crew/crew.component';
+import { DestinationsComponent } from './components/destinations/destinations.component';
+import { HomeComponent } from './components/home/home.component';
+import { TechnologyComponent } from './components/technology/technology.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"}, 
+  {path: "home", component: HomeComponent}, 
+  {path: "destination", component: DestinationsComponent}, 
+  {path: "crew", component: CrewComponent}, 
+  {path: "technology", component: TechnologyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
