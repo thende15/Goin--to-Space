92039ed3dda93e8c122d42f613f097dc39847dc9
Timothy Henderson <titansmith8@hotmail.com>
2022-10-06 12:37:34 -0400

    first comp set, plus service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { CrewComponent } from './components/crew/crew.component';
+import { TechnologyComponent } from './components/technology/technology.component';
+import { DestinationsComponent } from './components/destinations/destinations.component';
+
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainNavComponent,
+    CrewComponent, 
+    TechnologyComponent, 
+    DestinationsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  firebaseURL = 'https://goin--to-space-default-rtdb.firebaseio.com/'
+  ext = '.json'
+  constructor() { }
+  //crew
+  //destinations
+  //technology
+
+}
