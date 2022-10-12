c59102f6328524d57142219af4235b38b2f2b5c1
Timothy Henderson <titansmith8@hotmail.com>
2022-10-12 13:03:12 -0400

    modal finished

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -13,42 +13,33 @@ export class MainNavComponent implements OnInit {
   }
 
   toggleNavModal(){
-    const modal = document.getElementById('navModal')
-    const backDrop = document.getElementById('backDrop')
-    const hamburger = document.getElementById('hamburger')
-    const closeButton = document.getElementById('closeButton')
-    hamburger?.classList.toggle('displayNone');
-    hamburger?.classList.toggle('displayBlock');
-    closeButton?.classList.toggle('displayNone');
-    closeButton?.classList.toggle('displayBlock');
-    backDrop?.classList.toggle('displayNone');
-    backDrop?.classList.toggle('displayBlock');
-    modal?.classList.toggle('displayNone');
-    modal?.classList.toggle('displayFlex');
+    // const modal = document.getElementById('navModal')
+    // const backDrop = document.getElementById('backDrop')
+    // const hamburger = document.getElementById('hamburger')
+    // const closeButton = document.getElementById('closeButton')
+    // hamburger?.classList.toggle('displayNone');
+    // hamburger?.classList.toggle('displayBlock');
+    // closeButton?.classList.toggle('displayNone');
+    // closeButton?.classList.toggle('displayBlock');
+    // backDrop?.classList.toggle('displayNone');
+    // backDrop?.classList.toggle('displayBlock');
+    // modal?.classList.toggle('displayNone');
+    // modal?.classList.toggle('displayFlex');
+    document.getElementById("mySidenav")?.classList.toggle('displayBlock');
+    document.getElementById("mySidenav")?.classList.toggle('displayNone');
+    document.getElementById("hamburger")?.classList.toggle('displayBlock');
+    document.getElementById("hamburger")?.classList.toggle('displayNone');
+    document.getElementById("closeButton")?.classList.toggle('displayBlock');
+    document.getElementById("closeButton")?.classList.toggle('displayNone');
+
   }
-  // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+  // function openNav() {
+  //   
+  //   document.getElementById("main").style.marginLeft = "250px";
+  // }
+  
+  // function closeNav() {
+  //   document.getElementById("mySidenav").style.width = "0";
+  //   document.getElementById("main").style.marginLeft= "0";
+  // }
 }
