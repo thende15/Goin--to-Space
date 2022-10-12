1e98733c36d1bece116096631c7d8f704dc1e857
Timothy Henderson <titansmith8@hotmail.com>
2022-10-12 10:04:50 -0400

    begin route active work, modal tinkering

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -12,4 +12,18 @@ export class MainNavComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleNavModal(){
+    const modal = document.getElementById('navModal')
+    const backDrop = document.getElementById('backDrop')
+    const hamburger = document.getElementById('hamburger')
+    const closeButton = document.getElementById('closeButton')
+    hamburger?.classList.toggle('displayNone');
+    hamburger?.classList.toggle('displayBlock');
+    closeButton?.classList.toggle('displayNone');
+    closeButton?.classList.toggle('displayBlock');
+    backDrop?.classList.toggle('displayNone');
+    backDrop?.classList.toggle('displayBlock');
+    modal?.classList.toggle('displayNone');
+    modal?.classList.toggle('displayFlex');
+  }
 }
